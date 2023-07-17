a2854b8a8748a961a1e6a80b104a07b8430fd59e
BendUOverr <luka.buadzee@gmail.com>
2023-07-18 01:14:20 +0200

    feat:HealthcareProv

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from './components/Header'
 import { OurServices } from './components/OurServices'
+import { HealthcareProv } from './components/HealthcareProv'
 
 export const App = () => {
   return (
   <div>
    <Header /> 
    <OurServices />    
+   <HealthcareProv />
     </div>
   )
 }
---
+++ src/components/HealthcareProv.js
@@ -0,0 +1,40 @@
+import React from "react";
+import image8 from "../Assets/image8.png";
+import Dotss from "../Assets/element.png";
+
+export const HealthcareProv = () => {
+  return (
+    <div className="flex justify-center h-screen items-center">
+      <div className="max-w-[1400px] w-full ">
+        <div className="flex gap-10">
+          {/* img */}
+          <div>
+            <div>
+              <img src={image8} />
+            </div>
+          </div>
+          <div className="flex items-center pl-20">
+            <div className="flex w-[490px] h-[390px] flex-col gap-10">
+              <h1 className="font-bold text-4xl pr-10">
+                Leading healthcare providers
+              </h1>
+              <p className="pr-20 opacity-50">
+                Trafalgar provides progressive, and affordable healthcare,
+                accessible on mobile and online for everyone. To us, it’s not
+                just work. We take pride in the solutions we deliver
+              </p>
+              <div className="rounded-full border-[#458FF6] w-[180px] flex justify-center border-2">
+                <button className="text-[#458FF6] p-3 font-bold">
+                  Learn more
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute right-0">
+          <img src={Dotss} />
+        </div>
+      </div>
+    </div>
+  );
+};
